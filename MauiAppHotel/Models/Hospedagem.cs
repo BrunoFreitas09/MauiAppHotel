e9c4ffd9727929d4fcdd2d007b53dbd0218a92d7
Usuario <Usuario@CPD03-M18>
2024-06-10 16:00:12 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/Models/Hospedagem.cs
@@ -0,0 +1,29 @@
+namespace MauiAppHotel.Models;
+
+public class Hospedagem 
+{
+	public Quarto Quartoselecionado { get; set; }
+	public int QntAdultos { get; set; }
+	public int QntCriancas {  get; set; }
+	public DateTime DataCheckIn { get; set; }
+	public DateTime DataCheckOut { get; set; }
+	public int Estadia
+	{
+		get => DataCheckOut.Subtract(DataCheckIn).Days;
+		
+	}
+		public double ValorTotal
+	{
+		get
+		{
+			double valor_adultos = QntAdultos * Quartoselecionado.ValorDiariaAdulto;
+			double valor_criancas = QntCriancas * Quartoselecionado.ValorDiariaCrianca;
+
+			double total = (valor_adultos + valor_criancas) * Estadia;
+
+			return total;
+		}
+	}
+	
+	
+}
---
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,14 @@
+namespace MauiAppHotel.Views;
+
+public partial class ContratacaoHospedagem : ContentPage
+{
+	public ContratacaoHospedagem()
+	{
+		InitializeComponent();
+	}
+
+    private void dtck_checkin_DateSelected(object sender, DateChangedEventArgs e)
+    {
+
+    }
+}
